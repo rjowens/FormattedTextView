36df7e0d35859f70bb512cf38f2791c9b6df410e
Richard Owens <richardowens@new-host-3.home>
2012-05-16 21:24:28 -0400

    cleanup

--- app/controller/root_view_controller.rb
+++ app/controller/root_view_controller.rb
@@ -4,32 +4,37 @@ class RootViewController < UIViewController
 		self.view.backgroundColor = ColorHelper::rgbaStringToUIColor("rgba(213,222,217,1)")
 		self.tabBarItem = UITabBarItem.alloc.initWithTitle("Method 1", image:nil, tag:0)
 
-		formattedTextView = FormattedTextView.alloc.initWithFrame([[10, 20], [self.view.bounds.size.width - 20, self.view.size.height - 20]])
-    formattedTextView.layer.cornerRadius = 2
-		formattedTextView.backgroundColor = UIColor.whiteColor
-		formattedTextView.padding= [10,10,10,10]
-		formattedTextView.formattedText = "{@AmericanTypewriter:16:rgba(0,0,0,1.0)}Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-		formattedTextView.frame = [formattedTextView.frame.origin, [formattedTextView.frame.size.width, formattedTextView.textHeight]]
-    formattedTextView.layer.borderColor = ColorHelper::rgbaStringToUIColor("rgba(124,106,81,1)").CGColor
-    formattedTextView.layer.borderWidth = 1.0
-		self.view.addSubview(formattedTextView)
+		self.view.addSubview begin
+      @ftv = FormattedTextView.alloc.initWithFrame([[10, 20], [self.view.bounds.size.width - 20, self.view.size.height - 20]])
+      @ftv.layer.cornerRadius = 2
+		  @ftv.backgroundColor = UIColor.whiteColor
+		  @ftv.padding= [10,10,10,10]
+		  @ftv.formattedText = "{@AmericanTypewriter:16:rgba(0,0,0,1.0)}Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+		  @ftv.frame = [@ftv.frame.origin, [@ftv.frame.size.width, @ftv.textHeight]]
+      @ftv.layer.borderColor = ColorHelper::rgbaStringToUIColor("rgba(124,106,81,1)").CGColor
+      @ftv.layer.borderWidth = 1.0
+      @ftv
+		end
 
-    formattedTextView2 = FormattedTextView.alloc.initWithFrame([[10, formattedTextView.frame.origin.y + formattedTextView.frame.size.height + 15], [self.view.bounds.size.width - 20, self.view.size.height - 20]])
-    formattedTextView2.layer.cornerRadius = 2
-    formattedTextView2.backgroundColor = UIColor.whiteColor
-    formattedTextView2.padding= [10,10,10,10]
-    formattedTextView2.formattedText = "{@AmericanTypewriter:16:rgba(128,0,0,1.0)}Lorem ipsum{@AmericanTypewriter:16:rgba(0,0,0,1.0)} dolor sit amet, {@AmericanTypewriter-Bold:16:rgba(0,0,0,1.0)}consectetur{@AmericanTypewriter:16:rgba(0,0,0,1.0)} adipiscing elit."
-    formattedTextView2.frame = [formattedTextView2.frame.origin, [formattedTextView2.frame.size.width, formattedTextView2.textHeight]]
-    formattedTextView2.layer.borderColor = ColorHelper::rgbaStringToUIColor("rgba(124,106,81,1)").CGColor
-    formattedTextView2.layer.borderWidth = 1.0
-    self.view.addSubview(formattedTextView2)
+    self.view.addSubview begin
+      @ftv2 = FormattedTextView.alloc.initWithFrame([[10, @ftv.frame.origin.y + @ftv.frame.size.height + 15], [self.view.bounds.size.width - 20, self.view.size.height - 20]])
+      @ftv2.layer.cornerRadius = 2
+      @ftv2.backgroundColor = UIColor.whiteColor
+      @ftv2.padding= [10,10,10,10]
+      @ftv2.formattedText = "{@AmericanTypewriter:16:rgba(128,0,0,1.0)}Lorem ipsum{@AmericanTypewriter:16:rgba(0,0,0,1.0)} dolor sit amet, {@AmericanTypewriter-Bold:16:rgba(0,0,0,1.0)}consectetur{@AmericanTypewriter:16:rgba(0,0,0,1.0)} adipiscing elit."
+      @ftv2.frame = [@ftv2.frame.origin, [@ftv2.frame.size.width, @ftv2.textHeight]]
+      @ftv2.layer.borderColor = ColorHelper::rgbaStringToUIColor("rgba(124,106,81,1)").CGColor
+      @ftv2.layer.borderWidth = 1.0      
+      @ftv2
+    end
 
-    formattedTextView3 = FormattedTextView.alloc.initWithFrame([[10, formattedTextView2.frame.origin.y + formattedTextView2.frame.size.height + 15],
+    self.view.addSubview begin
+      @ftv3 = FormattedTextView.alloc.initWithFrame([[10, @ftv2.frame.origin.y + @ftv2.frame.size.height + 15],
                                                                 [self.view.bounds.size.width - 20, 280]])
-    formattedTextView3.layer.cornerRadius = 2
-    formattedTextView3.backgroundColor = UIColor.whiteColor
-    formattedTextView3.padding= [10,10,10,10]
-    formattedTextView3.formattedText = <<-eos
+      @ftv3.layer.cornerRadius = 2
+      @ftv3.backgroundColor = UIColor.whiteColor
+      @ftv3.padding= [10,10,10,10]
+      @ftv3.formattedText = <<-eos
     {@AmericanTypewriter:16:rgba(128,0,0,1.0)}Lorem ipsum{@AmericanTypewriter:16:rgba(0,0,0,1.0)}
 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor \
 incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud \
@@ -45,9 +50,10 @@ irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nul
 pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia \
 deserunt mollit anim id est laborum.
   eos
-    formattedTextView3.layer.borderColor = ColorHelper::rgbaStringToUIColor("rgba(124,106,81,1)").CGColor
-    formattedTextView3.layer.borderWidth = 1.0
-    self.view.addSubview(formattedTextView3)
+      @ftv3.layer.borderColor = ColorHelper::rgbaStringToUIColor("rgba(124,106,81,1)").CGColor
+      @ftv3.layer.borderWidth = 1.0
+      @ftv3
+    end
 
   end
 end
